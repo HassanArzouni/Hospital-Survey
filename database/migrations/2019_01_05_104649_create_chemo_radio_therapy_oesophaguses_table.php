ed1c830b8a6754bfdd57e7e7c0c65747e8cf4bb0
HassanArzouni <hassanarzouni@gmail.com>
2019-01-08 00:32:53 +0200

    Place foreign keys for tables that are done so far and finish chemo radio therapy table

--- database/migrations/2019_01_05_104649_create_chemo_radio_therapy_oesophaguses_table.php
+++ database/migrations/2019_01_05_104649_create_chemo_radio_therapy_oesophaguses_table.php
@@ -15,6 +15,32 @@ class CreateChemoRadioTherapyOesophagusesTable extends Migration
     {
         Schema::create('chemo_radio_therapy_oesophaguses', function (Blueprint $table) {
             $table->increments('id');
+
+            $table->boolean('has_nutritional_support');
+            $table->enum('nutritional_support_details', [
+                'Nasogastric tube',
+                'Gastrostomy',
+                'Jejunostomy'
+            ]);
+
+            $table->json('type_of_therapy');
+
+            $table->json('scheme_of_therapy');
+
+            $table->boolean('is_therapy_aborted');
+
+            $table->boolean('had_complications_during_therapy');
+            $table->json('complications_during_therapy_details');
+
+            $table->boolean('is_moc_performed');
+            $table->date('moc_performed_date');
+
+            $table->json('reevaluation_moc');
+
+            $table->string('clinical_re-staging_ct');
+            $table->string('clinical_re-staging_cn');
+            $table->string('clinical_re-staging_cm');
+
             $table->timestamps();
         });
     }
--- database/migrations/2019_01_05_105805_create_oesophagus_diagnosis_forms_table.php
+++ database/migrations/2019_01_05_105805_create_oesophagus_diagnosis_forms_table.php
@@ -16,16 +16,35 @@ class CreateOesophagusDiagnosisFormsTable extends Migration
         Schema::create('oesophagus_diagnosis_forms', function (Blueprint $table) {
             $table->increments('id');
 
+            $table->unsignedInteger('comorbidity_history_oesophaguse_id');
+            $table->foreign('comorbidity_history_oesophaguse_id')->references('id')->on('comorbidity_history_oesophaguses');
 
-            // id from comorbidity_history_oesophaguses
-            // id from endoscopy_biopsy_oesophaguses
-            // id from endoscopic_ultrasound_oesophaguses
-            // id from endoscopic_resection_oesophaguses
-            // id from tomography_ultrasound_oesophaguses
-            // clinical_staging_oesophaguses
-            // complications_oesophaguses
-            // treatment_plan_oesophaguses
-            // endoscopic_resection_therapy_oesophaguses
+            $table->unsignedInteger('endoscopy_biopsy_oesophaguse_id');
+            $table->foreign('endoscopy_biopsy_oesophaguse_id')->references('id')->on('endoscopy_biopsy_oesophaguses');
+
+            $table->unsignedInteger('endoscopic_ultrasound_oesophaguse_id');
+            $table->foreign('endoscopic_ultrasound_oesophaguse_id')->references('id')->on('endoscopic_ultrasound_oesophaguses');
+
+            $table->unsignedInteger('endoscopic_resection_oesophaguse_id');
+            $table->foreign('endoscopic_resection_oesophaguse_id')->references('id')->on('endoscopic_resection_oesophaguses');
+
+            $table->unsignedInteger('tomography_ultrasound_oesophaguse_id');
+            $table->foreign('tomography_ultrasound_oesophaguse_id')->references('id')->on('tomography_ultrasound_oesophaguses');
+
+            $table->unsignedInteger('clinical_staging_oesophaguse_id');
+            $table->foreign('clinical_staging_oesophaguse_id')->references('id')->on('clinical_staging_oesophaguses');
+
+            $table->unsignedInteger('complications_oesophaguse_id');
+            $table->foreign('complications_oesophaguse_id')->references('id')->on('complications_oesophaguses');
+
+            $table->unsignedInteger('treatment_plan_oesophaguse_id');
+            $table->foreign('treatment_plan_oesophaguse_id')->references('id')->on('treatment_plan_oesophaguses');
+
+            $table->unsignedInteger('endoscopic_resection_therapy_oesophaguse_id');
+            $table->foreign('endoscopic_resection_therapy_oesophaguse_id')->references('id')->on('endoscopic_resection_therapy_oesophaguses');
+
+            $table->unsignedInteger('chemo_radio_therapy_oesophaguse_id');
+            $table->foreign('chemo_radio_therapy_oesophaguse_id')->references('id')->on('chemo_radio_therapy_oesophaguses');
 
 
 
@@ -41,6 +60,19 @@ class CreateOesophagusDiagnosisFormsTable extends Migration
      */
     public function down()
     {
+        Schema::table('oesophagus_diagnosis_forms', function (Blueprint $table) {
+            $table->dropForeign(['comorbidity_history_oesophaguse_id']);
+            $table->dropForeign(['endoscopy_biopsy_oesophaguse_id']);
+            $table->dropForeign(['endoscopic_ultrasound_oesophaguse_id']);
+            $table->dropForeign(['endoscopic_resection_oesophaguse_id']);
+            $table->dropForeign(['tomography_ultrasound_oesophaguse_id']);
+            $table->dropForeign(['clinical_staging_oesophaguse_id']);
+            $table->dropForeign(['complications_oesophaguse_id']);
+            $table->dropForeign(['treatment_plan_oesophaguse_id']);
+            $table->dropForeign(['endoscopic_resection_therapy_oesophaguse_id']);
+            $table->dropForeign(['chemo_radio_therapy_oesophaguse_id']);
+        });
+
         Schema::dropIfExists('oesophagus_diagnosis_forms');
     }
 }
